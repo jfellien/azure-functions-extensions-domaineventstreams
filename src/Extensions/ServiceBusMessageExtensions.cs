da0be7e8e6586b04c8853fd34579b2913f591861
Janek Fellien <fellien@medialesson.de>
2022-01-28 20:12:02 +0100

    Added new ToDomainEvent Extension method to resolve the new ServiceBusReceivedMessage too

--- src/Extensions/ServiceBusMessageExtensions.cs
+++ src/Extensions/ServiceBusMessageExtensions.cs
@@ -43,5 +43,39 @@ namespace devCrowd.CustomBindings.EventSourcing.Extensions
 
             return domainEvent;
         }
+        
+        /// <summary>
+        /// Converts a ServiceBusMessage to a DomainEvent
+        /// </summary>
+        /// <param name="serviceBusMessage"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">If UserProperty does not contain a Event Type property</exception>
+        /// <exception cref="ArgumentException">If Event Type not available in solution</exception>
+        public static IDomainEvent ToDomainEvent(this ServiceBusReceivedMessage serviceBusMessage)
+        {
+            if (serviceBusMessage.ApplicationProperties.ContainsKey(EVENT_TYPE) == false)
+            {
+                throw new ArgumentException($"ServiceBusMessage does not contain Application Property '{EVENT_TYPE}'");
+            }
+            
+            var eventTypeName = serviceBusMessage.ApplicationProperties[EVENT_TYPE].ToString();
+
+            if (string.IsNullOrEmpty(eventTypeName))
+            {
+                throw new ArgumentException($"Application Property '{EVENT_TYPE}' is null or empty");
+            }
+
+            var eventType = Type.GetType(eventTypeName, false, true);
+
+            if (eventType == null)
+            {
+                throw new ArgumentException($"Can't find Event Type: {eventTypeName} in current solution.");
+            }
+
+            var messageAsString = Encoding.UTF8.GetString(serviceBusMessage.Body);
+            var domainEvent = JsonConvert.DeserializeObject(messageAsString, eventType) as IDomainEvent;
+
+            return domainEvent;
+        }
     }
 }
