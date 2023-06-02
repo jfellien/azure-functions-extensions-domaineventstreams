d34e600e6a8b36c0e932bf2183a25dc46eafb875
Janek Fellien <fellien@medialesson.de>
2023-06-02 13:56:43 +0200

    use the default namings for connection strings when it is used as managed identity

--- src/devCrowd.CustomBindings.EventSourcing/DomainEventStreamBindingConfiguration.cs
+++ src/devCrowd.CustomBindings.EventSourcing/DomainEventStreamBindingConfiguration.cs
@@ -18,13 +18,15 @@ public class DomainEventStreamBindingConfiguration : IExtensionConfigProvider
 
     private static DomainEventStream GetFromAttribute(DomainEventStreamAttribute attribute)
     {
-        string eventStoreConnectionString = Environment.GetEnvironmentVariable("EVENT_STORE_CONNECTION_STRING");
+        string eventStoreConnectionString = Environment.GetEnvironmentVariable("EVENT_STORE_CONNECTION_STRING")
+                                            ?? Environment.GetEnvironmentVariable("EVENT_STORE__accountEndpoint");
         string eventStoreDatabaseName = Environment.GetEnvironmentVariable("EVENT_STORE_DB_NAME");
         string eventsCollectionName = Environment.GetEnvironmentVariable("DOMAIN_EVENTS_COLLECTION_NAME");
             
         if (string.IsNullOrEmpty(eventStoreConnectionString))
         {
-            throw new ArgumentException("EVENT_STORE_CONNECTION_STRING not set in Application Settings");
+            throw new ArgumentException("EVENT_STORE Connection String not set in Application Settings. " +
+                                        "Needs EVENT_STORE_CONNECTION_STRING or for managed identity EVENT_STORE__accountEndpoint");
         }
             
         IReadAndWriteDomainEvents domainEventStreamStorage = DomainEventStreamStorageLibrary.GetInstanceBy(
@@ -38,11 +40,13 @@ public class DomainEventStreamBindingConfiguration : IExtensionConfigProvider
                 $"Unexpected type of Connection String (starts with: '{eventStoreConnectionString[..15]}'). Can not instantiate a DomainEventStream Storage. Please fix the Connection String or use only a Sql Server or CosmosDB Connection String.");
         }
             
-        string serviceBusConnectionString = Environment.GetEnvironmentVariable("EVENT_HANDLER_CONNECTION_STRING");
+        string serviceBusConnectionString = Environment.GetEnvironmentVariable("EVENT_HANDLER_CONNECTION_STRING")
+                                            ??Environment.GetEnvironmentVariable("EVENT_HANDLER__fullyQualifiedNamespace");
             
         if (string.IsNullOrEmpty(serviceBusConnectionString))
         {
-            throw new ArgumentException("EVENT_HANDLER_CONNECTION_STRING not set in Application Settings");
+            throw new ArgumentException("EVENT_HANDLER Connection String not set in Application Settings. " +
+                                        "Needs EVENT_HANDLER_CONNECTION_STRING or for managed identity EVENT_HANDLER__fullyQualifiedNamespace");
         }
             
         ServiceBusDomainEventsPublisher domainEventsPublisher = new (
